9f55eeed099d16efb3c720ccf9dd2213d74d502c
Florian KOLBL <florian.kolbl@gmail.com>
2026-03-06 13:59:36 +0100

    test for RTD. FK

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
